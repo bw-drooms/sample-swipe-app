6ac438188e345ca6c3c69c8a44c873521cd9ad63
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-22 10:30:30 -0500

    k

--- swiper/src/components/BtmNavBar.js
+++ swiper/src/components/BtmNavBar.js
@@ -5,7 +5,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import CommentIcon from '@material-ui/icons/Comment';
 const useStyles = makeStyles({
   root: {
     display:"flex",
@@ -26,9 +26,9 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Message" icon={<CommentIcon />} />
       <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Location" icon={<LocationOnIcon />} />
     </BottomNavigation>
   );
 }
